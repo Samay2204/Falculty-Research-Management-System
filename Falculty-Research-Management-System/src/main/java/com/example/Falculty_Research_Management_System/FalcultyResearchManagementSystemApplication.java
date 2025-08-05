0482b5c7c6b921e30a7b55635f73be3d72ee82ec
Samay2204 <shauryanamdeo2204@gmail.com>
2025-08-06 00:09:10 +0530

    first commit

---
+++ Falculty-Research-Management-System/src/main/java/com/example/Falculty_Research_Management_System/FalcultyResearchManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Falculty_Research_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalcultyResearchManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalcultyResearchManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Falculty-Research-Management-System/src/test/java/com/example/Falculty_Research_Management_System/FalcultyResearchManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Falculty_Research_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalcultyResearchManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
